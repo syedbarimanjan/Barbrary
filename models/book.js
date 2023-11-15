cafe7d152db93bab1539274bc731199ec7f4a49c
syedbarimanjan <syedbarimanjan@gmail.com>
2023-11-15 00:33:57 -0800

    uploading book cover image using filepond

--- models/book.js
+++ models/book.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const path = require("path")
 
-const coverImageBasePath="uploads/bookCover";
 
 const bookSchema = new mongoose.Schema({
     title:{
@@ -24,9 +22,13 @@ const bookSchema = new mongoose.Schema({
         required:true,
         default:Date.now,
     },
-    coverImageName:{
-        type:String,
+    coverImage:{
+        type:Buffer,
         requierd:true,
+    },
+    coverImageType:{
+        type:String,
+        reqiured:true,
     },
     author:{
         type: mongoose.Schema.Types.ObjectId,
@@ -36,10 +38,9 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual("coverImagePath",).get(function() {
-    if(this.coverImageName != null){
-        return path.join("/",coverImageBasePath,this.coverImageName)
+    if(this.coverImage != null && this.coverImageType != null){
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString("base64")}`
     }
 })
 
 module.exports = mongoose.model("Books",bookSchema);
-module.exports.coverImageBasePath = coverImageBasePath
